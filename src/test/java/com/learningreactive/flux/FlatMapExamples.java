0cb80dc27f880fb4cc492b9f1458394d6ec39398
harish sarapalle <hari328@live.com>
2019-11-21 09:04:11 +0530

    added parallel flat map

--- src/test/java/com/learningreactive/flux/FlatMapExamples.java
+++ src/test/java/com/learningreactive/flux/FlatMapExamples.java
@@ -7,6 +7,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FlatMapExamples {
 
   @Test
@@ -19,9 +21,39 @@ public class FlatMapExamples {
       .verifyComplete();
   }
 
+  @Test
+  public void simpleParellelFlatMap() {
+    Flux<String> fluxData =
+      Flux.just(1, 2, 3, 5, 3,34, 45, 567, 457,23 ,23, 21,12 ,4,234)
+        .window(3) // Flux<Flux<Integer>>
+        .flatMap(eleOfThree -> eleOfThree.map(this::convertToList)).subscribeOn(parallel()) // Flux<List<String>>
+        .flatMap(Flux::fromIterable)   // Flux<String>
+        .log();
+
+    StepVerifier.create(fluxData)
+      .expectSubscription()
+      .expectNextCount(30)
+      .verifyComplete();
+  }
+
+  @Test
+  public void simpleParellelFlatMapSequential() {
+    Flux<String> fluxData =
+      Flux.just(1, 2, 3, 5, 3,34, 45, 567, 457,23 ,23, 21,12 ,4,234)
+        .window(3) // Flux<Flux<Integer>>
+        .flatMapSequential(eleOfThree -> eleOfThree.map(this::convertToList)).subscribeOn(parallel()) // Flux<List<String>>
+        .flatMap(Flux::fromIterable)   // Flux<String>
+        .log();
+
+    StepVerifier.create(fluxData)
+      .expectSubscription()
+      .expectNextCount(30)
+      .verifyComplete();
+  }
+
   private List<String> convertToList(Integer num) {
     try {
-      Thread.sleep(1000);
+      Thread.sleep(100);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
